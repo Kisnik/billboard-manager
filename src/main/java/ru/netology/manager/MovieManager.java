cf977f4339b0d313b48ced0445030f7cbf46eb71
Nikolai Kiselev <kisnikn@mail.ru>
2020-12-05 00:56:18 +0300

    MovieRepository was added, tests for new methods were created

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,84 +1,31 @@
 package ru.netology.manager;
 
 import ru.netology.domain.Movie;
+import ru.netology.repository.MovieRepository;
 
 public class MovieManager {
 
-    private Movie[] movies = new Movie[0];
+    private MovieRepository repository;
 
+    public MovieManager(MovieRepository repository) {
+        this.repository = repository;
+    }
 
     public void add(Movie item) {
-        // создаём новый массив размером на единицу больше
-        int length = movies.length + 1;
-        Movie[] tmp = new Movie[length];
-        System.arraycopy(movies, 0, tmp, 0, movies.length);
-        // кладём последним наш элемент
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        movies = tmp;
-
+        repository.save(item);
     }
 
     public Movie[] getAll() {
-        Movie[] result = new Movie[movies.length];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
+        Movie[] items = repository.findAll();
+        Movie[] result = new Movie[items.length];
         for (int i = 0; i < result.length; i++) {
-            int index = movies.length - i - 1;
-            result[i] = movies[index];
+            int index = items.length - i - 1;
+            result[i] = items[index];
         }
         return result;
     }
 
-    public Movie[] getMovies() {
-        int numToShow = Math.min(movies.length, 10);
-        Movie[] result = new Movie[numToShow];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
-        for (int i = 0; i < result.length; i++) {
-            int index = numToShow - i - 1;
-            result[i] = movies[index];
-        }
-        return result;
+    public void removeById(int id) {
+        repository.removeById(id);
     }
-
-    public Movie[] getMovies(int numOfMovies) {
-        Movie[] result;
-        //получить фильмов больше, чем есть в ленте
-        if (numOfMovies > movies.length) {
-            result = new Movie[movies.length];
-            // перебираем массив в прямом порядке
-            // но кладём в результаты в обратном
-            for (int i = 0; i < result.length; i++) {
-                int index = movies.length - i - 1;
-                result[i] = movies[index];
-            }
-        }
-        //получить фильмов меньше, чем есть в ленте
-        else {
-            result = new Movie[numOfMovies];
-            // перебираем массив в прямом порядке
-            // но кладём в результаты в обратном
-            for (int i = 0; i < numOfMovies; i++) {
-                int index = numOfMovies - i - 1;
-                result[i] = movies[index];
-            }
-
-        }
-        return result;
-    }
-/*  // наивная реализация
-  public void removeById(int id) {
-    int length = items.length - 1;
-    Movie[] tmp = new Movie[length];
-    int index = 0;
-    for (Movie item : items) {
-      if (item.getId() != id) {
-        tmp[index] = item;
-        index++;
-      }
-    }
-    // меняем наши элементы
-    items = tmp;
-  }*/
 }
--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -2,11 +2,13 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Movie;
+import ru.netology.repository.MovieRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieManagerTest {
-    private MovieManager movieManager = new MovieManager();
+    private MovieRepository movieRepository = new MovieRepository();
+    private MovieManager movieManager = new MovieManager(movieRepository);
     private Movie[] movies;
 
     //добавление фильма в ленту
@@ -26,75 +28,23 @@ class MovieManagerTest {
 
     }
 
-    //получить последние 10 фильмов, если их в ленте меньше 10
     @Test
-    public void shouldGetLessTen() {
+    public void shouldRemoveById() {
         movies = new Movie[3];
         for (int i = 0; i < 3; i++) {
             movies[i] = new Movie();
+            movies[i].setId(i);
         }
+        Movie[] expected = new Movie[]{movies[2], movies[0]};
         for (int i = 0; i < 3; i++) {
             movieManager.add(movies[i]);
         }
-        Movie[] actual = movieManager.getMovies();
-        Movie[] expected = new Movie[]{movies[2], movies[1], movies[0]};
+        movieManager.removeById(1);
+        Movie[] actual = movieManager.getAll();
 
         assertArrayEquals(expected, actual);
 
-    }
 
-    //получить последние 10 фильмов, если их в ленте больше 10
-    @Test
-    public void shouldGetIfMoreTen() {
-        movies = new Movie[12];
-        for (int i = 0; i < 12; i++) {
-            movies[i] = new Movie();
-        }
-        for (int i = 0; i < 12; i++) {
-            movieManager.add(movies[i]);
-        }
-        Movie[] actual = movieManager.getMovies();
-        Movie[] expected = new Movie[10];
-        for (int i = 0; i < 10; i++) {
-            expected[i] = movies[11 - i];
-        }
-        assertArrayEquals(expected, actual);
-    }
-
-    //получить произвольное число фильмов, которое больше, чем фильмов в ленте
-    @Test
-    public void shouldGetMoreThanInBillboard() {
-        movies = new Movie[3];
-        for (int i = 0; i < 3; i++) {
-            movies[i] = new Movie();
-        }
-        for (int i = 0; i < 3; i++) {
-            movieManager.add(movies[i]);
-        }
-        Movie[] actual = movieManager.getMovies(5);
-        Movie[] expected = new Movie[3];
-        for (int i = 0; i < 3; i++) {
-            expected[i] = movies[2 - i];
-        }
-        assertArrayEquals(expected, actual);
-    }
-
-    //получить произвольное число фильмов, которое меньше, чем фильмов в ленте
-    @Test
-    public void shouldGetLessThanInBillboard() {
-        movies = new Movie[3];
-        for (int i = 0; i < 3; i++) {
-            movies[i] = new Movie();
-        }
-        for (int i = 0; i < 3; i++) {
-            movieManager.add(movies[i]);
-        }
-        Movie[] actual = movieManager.getMovies(2);
-        Movie[] expected = new Movie[2];
-        for (int i = 0; i < 2; i++) {
-            expected[i] = movies[2 - i];
-        }
-        assertArrayEquals(expected, actual);
     }
 
 }
